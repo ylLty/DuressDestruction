77fceaa083bcd48bb16c0fb7e7c39345fd7439d4
ylLty <yl_lty@outlook.com>
2025-12-14 13:16:16 +0800

    修改了描述 添加了正式的仓库地址

--- duress_destruction/action.sh
+++ duress_destruction/action.sh
@@ -1,4 +1,4 @@
 #!/system/bin/sh
 
 # 打开默认浏览器访问 GitHub 仓库
-am start -a android.intent.action.VIEW -d "https://example.com"
+am start -a android.intent.action.VIEW -d "https://github.com/ylLty/DuressDestruction/"
